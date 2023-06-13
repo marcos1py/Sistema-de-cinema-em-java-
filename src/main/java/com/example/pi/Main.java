5a0ff6fa78fd07599b2eee61a1716fb8c3171f92
Aluno <marcosantoniop47@gmail.com>
2023-06-13 19:45:16 -0300

    Arrumei os controllers dos fxml e adicionei algumas classes na main

--- src/main/java/com/example/pi/Main.java
+++ src/main/java/com/example/pi/Main.java
@@ -8,6 +8,14 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Main extends Application {
+   private static Stage stage;
+   private static Scene telaLogin;
+   private static Scene pagUsuarioFilme;
+   private static Scene pagFilmeComprar;
+   private static Scene pagTipoIngresso;
+   private static Scene pagTipoPagamento;
+   private static Scene pagTipoPagamentoPainel;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("TelaLogin.fxml"));
