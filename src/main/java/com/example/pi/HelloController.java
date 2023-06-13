16f8c99fdd2f0a126828cf0eb8c1a0c38e620ce2
Marcos <marcosantoniop47@gmail.com>
2023-06-13 18:45:47 -0300

    s

--- src/main/java/com/example/pi/HelloController.java
+++ src/main/java/com/example/pi/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX !");
+        welcomeText.setText("Welcome to JavaFX 1 !");
     }
 }
