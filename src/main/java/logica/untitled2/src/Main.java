fe031cd47ba6c86e40ed1fbdea4bab033e4fd82f
Aluno <marcosantoniop47@gmail.com>
2023-06-13 19:11:34 -0300

    Adicionei a logica e os fxml

---
+++ src/main/java/logica/untitled2/src/Main.java
@@ -0,0 +1,29 @@
+package logica.untitled2.src;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while(true){
+            String nomeDoFilmeAC = "";
+            boolean fileira1[] = new boolean[10];
+            System.out.println("Qual filme vc quer cadastrasr");
+
+            nomeDoFilmeAC = sc.next();
+
+            String filmes[] = new String[10];
+            for(int index = 0; index <= filmes.length; index++){
+                if(filmes[index] != null){
+                    System.out.println(index);
+                    System.out.println(filmes[index]);
+                    filmes[index] = nomeDoFilmeAC;
+                    break;
+
+                }
+            }
+
+        }
+    }
+}
