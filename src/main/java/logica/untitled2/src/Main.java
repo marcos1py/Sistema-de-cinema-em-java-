5d342bf09bc8001e5591a6c15a6ddbf83d955043
isaluh <heloisamedeiros7@hotmail.com>
2023-06-13 21:09:02 -0300

    fazendo logica de cadastro

--- src/main/java/logica/untitled2/src/Main.java
+++ src/main/java/logica/untitled2/src/Main.java
@@ -4,26 +4,54 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
+        String nomeFilme = "";
+        String nomeDelet = "";
+        String filmes[] = new String[10];
+        int cont = 0;
+        int aux = 0;
 
-        while(true){
-            String nomeDoFilmeAC = "";
-            boolean fileira1[] = new boolean[10];
-            System.out.println("Qual filme vc quer cadastrasr");
+       while (cont != 3){
+           while (aux != 2){
+               System.out.print("Insira um filme: ");
+               nomeFilme = input.next();
 
-            nomeDoFilmeAC = sc.next();
+               for(int index = 0; index < filmes.length; index++){
+                   if(filmes[index] == null) {
+                       filmes[index] = nomeFilme;
+                       break;
+                   }
+               }
+               aux += 1;
+           }
 
-            String filmes[] = new String[10];
-            for(int index = 0; index <= filmes.length; index++){
-                if(filmes[index] != null){
-                    System.out.println(index);
-                    System.out.println(filmes[index]);
-                    filmes[index] = nomeDoFilmeAC;
-                    break;
+           aux = 0;
 
-                }
-            }
+           System.out.print("Nome do filme a deletar: ");
+           nomeDelet = input.next();
 
-        }
+           for(int index = 0; index < filmes.length; index++){
+               if(filmes[index] == null){
+                   index += 1;
+               }
+               else {
+                   if(filmes[index].equals(nomeDelet)){
+                       filmes[index] = null;
+                   }
+               }
+
+               if(index == filmes.length){
+                   break;
+               }
+               System.out.println(filmes[index]);
+           }
+           cont += 1;
+       }
+
+
+
+
+
+
     }
 }
