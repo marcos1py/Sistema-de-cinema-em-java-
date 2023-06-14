0cdb2deb1b8c60888dc4b57cb30eb4f7cc503c42
Marcos <marcosantoniop47@gmail.com>
2023-06-13 21:49:40 -0300

    Add "exibirFilmes"

--- src/main/java/logica/untitled2/src/MainLogica.java
+++ src/main/java/logica/untitled2/src/MainLogica.java
@@ -5,47 +5,50 @@ import java.util.Scanner;
 public class MainLogica {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String nomeFilme = "";
-        String nomeDelet = "";
-        String filmes[] = new String[10];
+        String[] filmes = new String[10];
 
         System.out.print("Insira um filme: ");
-        nomeFilme = input.next();
+        String nomeFilme = input.next();
         adicionarFilme(filmes, nomeFilme);
 
-
         System.out.print("Nome do filme a deletar: ");
-        nomeDelet = input.next();
+        String nomeDelet = input.next();
         deletarFilme(filmes, nomeDelet);
-        System.out.println(filmes[0]);
 
+        System.out.println("Lista de filmes:");
+        exibirFilmes(filmes);
     }
 
     public static void adicionarFilme(String[] filmes, String nomeFilme) {
         for (int index = 0; index < filmes.length; index++) {
             if (filmes[index] == null) {
                 filmes[index] = nomeFilme;
-                break;
+                System.out.println("Filme adicionado com sucesso!");
+                return;
             }
         }
+        System.out.println("A lista de filmes está cheia. Não é possível adicionar mais filmes.");
     }
 
     public static void deletarFilme(String[] filmes, String nomeDelet) {
-        int conta = 0;
+        boolean deletado = false;
         for (int index = 0; index < filmes.length; index++) {
-
-            if (filmes[index] == null) {
-                index += 1;
-            } else {
-                if (filmes[index].equals(nomeDelet)) {
-                    filmes[index] = null;
-                }
+            if (filmes[index] != null && filmes[index].equals(nomeDelet)) {
+                filmes[index] = null;
+                System.out.println("Filme removido com sucesso!");
+                deletado = true;
             }
+        }
+        if (!deletado) {
+            System.out.println("O filme não foi encontrado na lista.");
+        }
+    }
 
-            if (index == filmes.length) {
-                break;
+    public static void exibirFilmes(String[] filmes) {
+        for (int index = 0; index < filmes.length; index++) {
+            if (filmes[index] != null) {
+                System.out.println(filmes[index]);
             }
-
         }
     }
 }
