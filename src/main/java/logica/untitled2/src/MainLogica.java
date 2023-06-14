60c2c6d92e5ad08a96e2ea1547f8187c17af62b4
Marcos <marcosantoniop47@gmail.com>
2023-06-13 21:39:51 -0300

    criei funçoes

--- src/main/java/logica/untitled2/src/MainLogica.java
+++ src/main/java/logica/untitled2/src/MainLogica.java
@@ -8,50 +8,44 @@ public class MainLogica {
         String nomeFilme = "";
         String nomeDelet = "";
         String filmes[] = new String[10];
-        int cont = 0;
-        int aux = 0;
 
-       while (cont != 3){
-           while (aux != 2){
-               System.out.print("Insira um filme: ");
-               nomeFilme = input.next();
-
-               for(int index = 0; index < filmes.length; index++){
-                   if(filmes[index] == null) {
-                       filmes[index] = nomeFilme;
-                       break;
-                   }
-               }
-               aux += 1;
-           }
-
-           aux = 0;
-
-           System.out.print("Nome do filme a deletar: ");
-           nomeDelet = input.next();
-
-           for(int index = 0; index < filmes.length; index++){
-               if(filmes[index] == null){
-                   index += 1;
-               }
-               else {
-                   if(filmes[index].equals(nomeDelet)){
-                       filmes[index] = null;
-                   }
-               }
-
-               if(index == filmes.length){
-                   break;
-               }
-               System.out.println(filmes[index]);
-           }
-           cont += 1;
-       }
+        System.out.print("Insira um filme: ");
+        nomeFilme = input.next();
+        adicionarFilme(filmes, nomeFilme);
 
 
+        System.out.print("Nome do filme a deletar: ");
+        nomeDelet = input.next();
+        deletarFilme(filmes, nomeDelet);
+        System.out.println(filmes[0]);
 
+    }
 
+    public static void adicionarFilme(String[] filmes, String nomeFilme) {
+        for (int index = 0; index < filmes.length; index++) {
+            if (filmes[index] == null) {
+                filmes[index] = nomeFilme;
+                break;
+            }
+        }
+    }
 
+    public static void deletarFilme(String[] filmes, String nomeDelet) {
+        int conta = 0;
+        for (int index = 0; index < filmes.length; index++) {
 
+            if (filmes[index] == null) {
+                index += 1;
+            } else {
+                if (filmes[index].equals(nomeDelet)) {
+                    filmes[index] = null;
+                }
+            }
+
+            if (index == filmes.length) {
+                break;
+            }
+
+        }
     }
 }
